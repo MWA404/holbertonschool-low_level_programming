1f2e0ca041cf75db7ccba2209625de9d64e91483
MWA404 <muhannadalraddadi@gmail.com>
2026-03-01 12:05:02 +0000

    Fix 0-putchar.c to use write

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -6,14 +6,6 @@
  */
 int main(void)
 {
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
+	write(1, "_putchar\n", 9);
 	return (0);
 }
