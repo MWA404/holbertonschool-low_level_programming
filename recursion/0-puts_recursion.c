022fa85e49cb4afacdc515294707a1c1cfbeb5a9
MWA404 <muhannadalraddadi@gmail.com>
2026-03-10 10:13:18 +0000

    Fix _puts_recursion to use write

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * _puts_recursion - prints a string followed by a new line
@@ -9,9 +9,9 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		printf("\n");
+		write(1, "\n", 1);
 		return;
 	}
-	printf("%c", *s);
+	write(1, s, 1);
 	_puts_recursion(s + 1);
 }
