3133747619eaf0dede20f5e637a101a65fcd5c00
MWA404 <muhannadalraddadi@gmail.com>
2026-03-14 17:06:55 +0000

    Fix task 0: correct C file content

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,10 +1,8 @@
-cat > 0-whatsmyname.c << 'EOF'
 #include <stdio.h>
 
 int main(int argc, char *argv[])
 {
-    (void)argc;
-    printf("%s\n", argv[0]);
-    return (0);
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
 }
-EOF
