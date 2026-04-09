d858b1cbf0d4d62f0650ceb6b871136793483534
MWA404 <mmmo20148@gmail.com>
2026-04-09 11:47:42 +0000

    Fix variadic_functions: Task 3 - reduce if statements

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,15 +1,61 @@
 #include "variadic_functions.h"
 
 /**
+ * print_char - prints a char
+ * @args: va_list with the char
+ */
+static void print_char(va_list args)
+{
+	printf("%c", va_arg(args, int));
+}
+
+/**
+ * print_int - prints an int
+ * @args: va_list with the int
+ */
+static void print_int(va_list args)
+{
+	printf("%d", va_arg(args, int));
+}
+
+/**
+ * print_float - prints a float
+ * @args: va_list with the float
+ */
+static void print_float(va_list args)
+{
+	printf("%f", va_arg(args, double));
+}
+
+/**
+ * print_string - prints a string
+ * @args: va_list with the string
+ */
+static void print_string(va_list args)
+{
+	char *str;
+
+	str = va_arg(args, char *);
+	if (!str)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", str);
+}
+
+/**
  * print_all - prints anything
  * @format: list of types of arguments
  */
 void print_all(const char * const format, ...)
 {
 	va_list args;
-	unsigned int i;
-	char *str;
+	unsigned int i, j;
 	char *sep;
+	char types[] = "cifs";
+	void (*funcs[])(va_list) = {print_char, print_int, print_float,
+		print_string};
 
 	va_start(args, format);
 	i = 0;
@@ -17,28 +63,16 @@ void print_all(const char * const format, ...)
 
 	while (format && format[i])
 	{
-		if (format[i] == 'c')
+		j = 0;
+		while (types[j])
 		{
-			printf("%s%c", sep, va_arg(args, int));
-			sep = ", ";
-		}
-		if (format[i] == 'i')
-		{
-			printf("%s%d", sep, va_arg(args, int));
-			sep = ", ";
-		}
-		if (format[i] == 'f')
-		{
-			printf("%s%f", sep, va_arg(args, double));
-			sep = ", ";
-		}
-		if (format[i] == 's')
-		{
-			str = va_arg(args, char *);
-			if (!str)
-				str = "(nil)";
-			printf("%s%s", sep, str);
-			sep = ", ";
+			if (format[i] == types[j])
+			{
+				printf("%s", sep);
+				funcs[j](args);
+				sep = ", ";
+			}
+			j++;
 		}
 		i++;
 	}
