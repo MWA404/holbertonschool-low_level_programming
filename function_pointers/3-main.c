8b49747d0c49f57fa55ecd4f9b7e5ce72eb4d62b
MWA404 <mmmo20148@gmail.com>
2026-04-09 11:38:09 +0000

    Add function_pointers: Task 3 - calc

---
+++ function_pointers/3-main.c
@@ -0,0 +1,43 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - performs simple operations
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+	int (*f)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	f = get_op_func(argv[2]);
+	if (!f)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((strcmp(argv[2], "/") == 0 || strcmp(argv[2], "%") == 0) && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", f(num1, num2));
+
+	return (0);
+}
