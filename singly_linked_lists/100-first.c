713b07b5eb2334d8cf8c419f5f673b1709d31198
MWA404 <muhannadalraddadi@gmail.com>
2026-04-22 11:43:57 +0300

    Add singly_linked_lists: Task 5 - before main

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * before_main - prints a message before main is executed
+ */
+__attribute__((constructor))
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
