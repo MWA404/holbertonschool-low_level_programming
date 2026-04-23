77d776026324a256ab1273c2cfe87d5741cc5173
MWA404 <muhannadalraddadi@gmail.com>
2026-04-23 12:14:07 +0300

    Fix file_io: Task 1 - add const to text_content

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -8,7 +8,7 @@
  * @text_content: NULL terminated string to write to the file
  * Return: 1 on success, -1 on failure
  */
-int create_file(const char *filename, char *text_content)
+int create_file(const char *filename, const char *text_content)
 {
 	int fd;
 	int len;
