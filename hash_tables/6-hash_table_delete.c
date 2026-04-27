e648fc18937e1127459ef0c0653af3b7dcf48589
MWA404 <muhannadalraddadi@gmail.com>
2026-04-27 10:24:03 +0300

    Add hash_tables: Task 6 - hash_table_delete

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: the hash table to delete
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *tmp, *next;
+
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		tmp = ht->array[i];
+		while (tmp != NULL)
+		{
+			next = tmp->next;
+			free(tmp->key);
+			free(tmp->value);
+			free(tmp);
+			tmp = next;
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
